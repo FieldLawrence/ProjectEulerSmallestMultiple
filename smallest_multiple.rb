4de9581b5c3d09dc457b299d3fe55d26afea0f9a
Lawrence Field <lfield@bu.edu>
2017-03-21 15:04:31 -0400

    initial commit

---
+++ smallest_multiple.rb
@@ -0,0 +1,8 @@
+class SmallestMultiple
+  smallest_number = 1
+  until smallest_number % 20 == 0 && smallest_number % 19 == 0 && smallest_number % 18 == 0 && smallest_number % 17 == 0 && smallest_number % 16 == 0 && smallest_number % 15 == 0 && smallest_number % 14 == 0 && smallest_number % 13 == 0 && smallest_number % 12 == 0 && smallest_number % 11 == 0
+  smallest_number +=1
+end
+puts smallest_number
+end
+# Created by Lawrence Field 3/21/2017
